b459cfd462778b6f07b42a9305814c939e4f7a89
win <wintercoredev@gmail.com>
2018-10-13 09:51:01 +0700

    23: add blank window example

--- examples/BlankWindow/Program.cs
+++ examples/BlankWindow/Program.cs
@@ -8,6 +8,19 @@ namespace BlankWindow
         [STAThread]
         static void Main()
         {
+
+            OpenTK.Platform.Factory.GetCustomPlatformFactory = () => OpenTK.Platform.Egl.EglAngle.NewFactory();
+
+            Toolkit.Init(new ToolkitOptions
+            {
+                Backend = PlatformBackend.PreferNative,
+
+            });
+
+            OpenTK.Graphics.PlatformAddressPortal.GetAddressDelegate = OpenTK.Platform.Utilities.CreateGetAddress();
+
+
+
             var program = new Program();
             program.Run();
         }
